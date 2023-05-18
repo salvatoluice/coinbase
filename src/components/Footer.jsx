cb4712a18021769fbde33ce91680c8ab8d2f7feb
salvatoluice <salvatoluice5@gmail.com>
2023-05-18 09:40:20 +0300

    Footer changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,10 +4,10 @@ function Footer() {
       <footer>
         <div className="footer-content">
           <div className="footer-content__socials">
-            <i className="fa-brands fa-twitter"></i>
-            <i className="fa-brands fa-discord"></i>
-            <i className="fa-brands fa-facebook"></i>
-            <i className="fa-brands fa-youtube"></i>
+            <a style={{color: 'white'}} href="https://twitter.com/LuiceSalvato/"><i className="fa-brands fa-twitter"></i></a>
+            <a style={{color: 'white'}} href="https://github.com/salvatoluice"><i className="fa-brands fa-github"></i></a>
+            <a style={{color: 'white'}} href="https://linkedin.com/in/salvato-luis/"><i className="fa-brands fa-linkedin"></i></a>
+            <a style={{color: 'white'}} href="https://instagram.com/salvato_luiz"><i className="fa-brands fa-instagram"></i></a>
           </div>
           <div className="footer-content__text">
             <p>Privacy Policy</p>
